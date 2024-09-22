5ed55d275394466f098e75af723ce7a617b83588
acohon <showwinho@gmail.com>
2024-09-22 19:00:08 +0800

    can edit login user function in admin site, congrats

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import User
-# Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(User)
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+
+class UserAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'email', 'name')  # 使用 User 模型中实际存在的字段
+    list_display_links = ('id', 'email', 'name')  # 使用 User 模型中实际存在的字段
+    list_filter = ('email', 'name')  # 使用 User 模型中实际存在的字段
+    search_fields = ('email', 'name')  # 使用 User 模型中实际存在的字段
+    list_per_page = 25
+    resource_classes = [UserResource]
+
+admin.site.register(User, UserAdmin)  # 确保注册 UserAdmin
